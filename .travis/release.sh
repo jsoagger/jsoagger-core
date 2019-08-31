06663105df5f527df1171d2c3009142114c0d088
yvonjisoa <rmvonji@gmail.com>
2019-08-31 23:25:19 +0200

    can not get through javadoc, generate empty one 1

--- .travis/release.sh
+++ .travis/release.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "Deploying release version to SONATYPE"
-mvn clean deploy --settings .maven.xml -DskipTests=true -Dmaven.javadoc.skip=true -B -U -Prelease
+mvn -X clean deploy --settings .maven.xml -DskipTests=true -Dmaven.javadoc.skip=true -B -U -Prelease
 
 # log is truncated when build fails
 # try this work arround
