829e35eb44aa27cc88d3bbca52be9c0cc42c0011
yvonjisoa <rmvonji@gmail.com>
2019-08-31 15:57:14 +0200

    debug script

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-./install-jdk.sh --url https://d3pxv6yz143wms.cloudfront.net/11.0.3.7.1/amazon-corretto-11.0.3.7.1-linux-x64.tar.gz
+source ./install-jdk.sh --url https://d3pxv6yz143wms.cloudfront.net/11.0.3.7.1/amazon-corretto-11.0.3.7.1-linux-x64.tar.gz
 
 
 
