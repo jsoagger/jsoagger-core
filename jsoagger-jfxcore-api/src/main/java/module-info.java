412eafb0a6239d39bbdea79bda7a310c7bb3904d
yvonjisoa <rmvonji@gmail.com>
2020-01-16 21:28:01 +0100

    Updated to version 1.0.2-SNAPSHOT.

--- jsoagger-jfxcore-api/src/main/java/module-info.java
+++ jsoagger-jfxcore-api/src/main/java/module-info.java
@@ -8,16 +8,12 @@ module io.github.jsoagger.core.api {
 	exports io.github.jsoagger.jfxcore.api.components.annotation;
 	exports io.github.jsoagger.jfxcore.viewdef.json.xml.model.appcontext;
 	exports io.github.jsoagger.jfxcore.viewdef.json.xml.model;
-	exports io.github.jsoagger.jfxcore.viewdef.xml.model;
 	exports io.github.jsoagger.jfxcore.api.presenter;
 	exports io.github.jsoagger.jfxcore.viewdef.json.xml;
-	exports io.github.jsoagger.jfxcore.viewdef.xml;
 	exports io.github.jsoagger.jfxcore.api.wizard;
 	exports io.github.jsoagger.jfxcore.api.view;
-	exports io.github.jsoagger.jfxcore.viewdef.xml.model.appcontext;
 
 	requires io.github.jsoagger.core.bridge;
-	requires java.xml.bind;
 	requires javafx.base;
 	requires javafx.controls;
 	requires javafx.graphics;
